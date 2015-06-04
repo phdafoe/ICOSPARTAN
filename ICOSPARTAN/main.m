970b582ede5f44846aa5da515acb62eb15aeab1f
User <David@MacBook-Pro.local>
2015-06-04 13:55:20 +0200

    Initial Commit

---
+++ ICOSPARTAN/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ICOSPARTAN
+//
+//  Created by User on 4/6/15.
+//  Copyright (c) 2015 iCologic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
